c63a4b7bbc4e46c66c5036f22a83f65d7573960b
fmontes00 <fernandomontesdeoca93@gmail.com>
2023-01-27 13:14:25 -0300

    account model correction and twwets models

--- twitter_clone/accounts/models.py
+++ twitter_clone/accounts/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(AbstractUser):    
+class User(AbstractUser):    
     is_premium = models.BooleanField(default=False)
-    
+    follower_count = models.IntegerField(default=0)
+    following_count = models.IntegerField(default=0)
+
     def __str__(self):
         return self.username
 
 
 class Relationship(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete= models.CASCADE)
-    follower = models.ForeignKey(CustomUser)
-    following = models.ForeignKey(CustomUser)
+    user = models.ForeignKey("accounts.User", on_delete= models.CASCADE)
+    follower = models.ForeignKey("accounts.User")
+    following = models.ForeignKey("accounts.User")
 
--- twitter_clone/tweets/models.py
+++ twitter_clone/tweets/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tweet(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    content = models.CharField(max_length=100)
+    coment_of = models.OneToOneField(self, on_delete=models.CASCADE)
+    likes_count = models.IntegerField(default=0)
+    comments_count = models.IntegerField(default=0)
+
+
+class Like(models.Model):
+    user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE)
