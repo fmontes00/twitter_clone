a69ebfd2fb5ef00457882fd751f696049bf0cbe0
fmontes00 <fernandomontesdeoca93@gmail.com>
2023-01-27 10:42:19 -0300

    apps creation

--- twitter_clone/accounts/models.py
+++ twitter_clone/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):    
+    is_premium = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+
+
+class Relationship(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete= models.CASCADE)
+    follower = models.ForeignKey(CustomUser)
+    following = models.ForeignKey(CustomUser)
+
