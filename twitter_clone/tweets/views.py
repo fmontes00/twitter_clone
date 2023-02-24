573444ef08b3d359c0e9b9c058f93e1d27ae2f18
fmontes00 <fernandomontesdeoca93@gmail.com>
2023-02-24 00:16:17 -0300

    api configuration for twittet_clon level

--- twitter_clone/tweets/views.py
+++ twitter_clone/tweets/views.py
@@ -4,4 +4,8 @@ from django.shortcuts import render
 
 
 
-# Create your views here.
+
+
+
+
+
--- twitter_clone/twitter_clone/settings.py
+++ twitter_clone/twitter_clone/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "accounts",
     "tweets",
+    "api",
     ]
 
 MIDDLEWARE = [
