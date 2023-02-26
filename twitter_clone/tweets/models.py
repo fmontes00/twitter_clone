e70cbc7f938f38584b2bad5123e5151476ef2760
fmontes00 <fernandomontesdeoca93@gmail.com>
2023-02-26 00:52:42 -0300

    coment field adition

--- twitter_clone/tweets/models.py
+++ twitter_clone/tweets/models.py
@@ -5,6 +5,11 @@ class Tweet(models.Model):
     user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     content = models.CharField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True, null=True)
+    coment = models.ForeignKey('Tweet', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
+
 
 
 class Like(models.Model):
