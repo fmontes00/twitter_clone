888b61cd8b4ae1419b5d7e107c4320768131a0a1
fmontes00 <fernandomontesdeoca93@gmail.com>
2023-03-25 17:17:42 -0300

    include in api url

--- twitter_clone/api/urls.py
+++ twitter_clone/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
-from .views import TwitterList, TweetCreation, TweetRetriveDestroyUpdate, LikeList, LikeCreation, LikeUpdateRetriveDelete
+from rest_framework.routers import DefaultRouter
+from .views import  LikeList, LikeCreation, LikeUpdateRetriveDelete, TweetViewSet             # TwitterList, TweetCreation, TweetRetriveDestroyUpdate,  
+
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"tweets", TweetViewSet, basename="tweet")
 
 urlpatterns = [
-    path("", TwitterList.as_view(), name="tweets_list"),
-    path("create-tweet/", TweetCreation.as_view(), name="tweet_creation"),
-    path("retrive-update-delete/<int:pk>/", TweetRetriveDestroyUpdate.as_view(), name="tweet_up_del_re"),
+    #path("", TwitterList.as_view(), name="tweets_list"),
+    #path("create-tweet/", TweetCreation.as_view(), name="tweet_creation"),
+    #path("retrive-update-delete/<int:pk>/", TweetRetriveDestroyUpdate.as_view(), name="tweet_up_del_re"),
+    path("",include(router.urls)),
     path("like-list/", LikeList.as_view(), name="like_list"),
     path("like-create/", LikeCreation.as_view(), name="like_create"),
     path("like-retrive-update-delete/<int:pk>/", LikeUpdateRetriveDelete.as_view(), name="like_up_del_re"),
